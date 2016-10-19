e982c0cb971339a2aef19addc6ff1e422e87f17b
Willian Lopes <willianlopes25@outlook.com>
2016-10-19 17:02:06 -0200

    Primeira versão

---
+++ index.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <title>RWD - Tableless</title>
+        
+
+        <link rel="stylesheet" href="css/style.css"/>
+        <link href='https://fonts.googleapis.com/css?family=Roboto:100,300,400,700' rel='stylesheet' type='text/css'>
+    </head>
+    <body>
+        <header>
+            <div class="container">
+                <h1 class="fontzero">RWD - Tableless </h1>
+                <ul class="main_menu">
+                    <li><a href="#" title="">Home</a></li>
+                    <li><a href="#" title="">Produtos</a></li>
+                    <li><a href="#" title="">Sobre Nós</a></li>
+                    <li><a href="#" title="">Contato</a></li>
+                </ul>
+                <div class="clear"></div>
+            </div>    
+        </header>
+        
+        <main>
+            <article class="container principal">
+                <div class="content">
+                    <h1 class="title">Uma frase bela</h1>
+                    <p class="tagline"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, natus est sit sunt ipsam deleniti modi eligendi.</p>
+                    <a class="btn" href="#" title="">Compre agora</a>
+                    <div class="clear"></div>
+                </div>
+            </article>    
+            
+            <!--Seção 1-->
+            <section class="section1 container">
+                <div class="content">
+                        <header>
+                            <h1 class="title">Laborum rem facilis maximie nostrum</h1>
+                            <p class="tagline">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum, rem, facilis, maxime nostrum asperiores molestiae eum dolor voluptatum ab officiis distinctio.</p>
+                        </header>
+                        
+                        <article>
+                            <h1 class="title2">Crafted with love</h1>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, debitis, at alias fugiat optio aliquam accusamus accusantium enim soluta aspernatur vero sed aperiam quasi porro cum nihil adipisci aut mollitia.</p>
+                            <a class="btn">Leia Mais</a>
+                        </article>
+                        
+                        <article>
+                            <h1 class="title2">Iure debits fugiat optio aliquam</h1>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, debitis, at alias fugiat optio aliquam accusamus accusantium enim soluta aspernatur vero sed aperiam quasi porro cum nihil adipisci aut mollitia.</p>
+                            <a class="btn">Saiba Mais</a>
+                        </article>
+                        
+                        <article>
+                            <h1 class="title2">Crafted with love</h1>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, debitis, at alias fugiat optio aliquam accusamus accusantium enim soluta aspernatur vero sed aperiam quasi porro cum nihil adipisci aut mollitia.</p>
+                            <a class="btn">Compre Agora</a>
+                        </article>
+                        
+                        <article>
+                            <h1 class="title2">Lorem ipsum dolor</h1>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, debitis, at alias fugiat optio aliquam accusamus accusantium enim soluta aspernatur vero sed aperiam quasi porro cum nihil adipisci aut mollitia.</p>
+                            <a class="btn">Leia Mais</a>
+                        </article>
+                        
+                        <article>
+                            <h1 class="title2">Crafted with love</h1>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, debitis, at alias fugiat optio aliquam accusamus accusantium enim soluta aspernatur vero sed aperiam quasi porro cum nihil adipisci aut mollitia.</p>
+                            <a class="btn">Saiba Mais</a>
+                        </article>
+                        
+                        <article>
+                            <h1 class="title2">Lorem ipsum dolor</h1>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, debitis, at alias fugiat optio aliquam accusamus accusantium enim soluta aspernatur vero sed aperiam quasi porro cum nihil adipisci aut mollitia.</p>
+                            <a class="btn">Compre Agora</a>
+                        </article>
+                        <div class="clear"></div>
+                </div>
+            </section>  
+            
+            
+            <!--Seção 2-->
+             <section class=" section2 container bg-gray" style="background-color: #F0EEED;">
+                <div class="content">
+                    <header>
+                        <h1 class="title">Laborum rem facilis maximie nostrum</h1>
+                        <p class="tagline"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum, rem, facilis, maxime nostrum asperiores molestiae eum dolor voluptatum ab officiis distinctio.</p>
+                    </header>
+                    
+                    <article>
+                        <img src="img/img1.jpg" title="" alt="">
+                        <h1 class="title3">Only Use what is necessary</h1>
+                        <p class="tagline"> Consectetur adipisicing elit. Asperiores dolore quisquam eum ducimus officiis odio ipsum modi.</p>
+                        <a href="" title="">Um link de chamada para a página</a>
+                    </article>
+                    
+                    <article>
+                        <img src="img/img2.jpg" title="" alt="">
+                        <h1 class="title3">Responsive Workshop do Tableless</h1>
+                        <p class="tagline"> Consectetur adipisicing elit. Asperiores dolore quisquam eum ducimus officiis odio ipsum modi.</p>
+                        <a href="" title="">Um link de chamada para a página</a>
+                    </article>
+                    
+                    <div class="clear"></div>
+                </div>
+            </section>  
+           
+            <!--Dobra Final-->
+            <article class="final container">
+                <div class="content">
+                    <img src="img/img3.jpg" alt="" title="">
+                    <header>
+                        <h1 class="title">Website Responsive Web Design Layout</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque, animi rerum cupiditate tenetur quod illo facere molestias explicabo cumque dicta magni est qui distinctio esse officiis inventore voluptas harum quos.</p>
+                    </header>
+                    <div class="clear"></div>
+                </div>
+            </article>
+        </main>
+        
+        <footer class="main_footer container">
+            <a href="#" title="">Tableless.com.br</a><br>
+            <span>Footer do site onde vai o Copyright e as outras coisas.</span>
+        </footer>
+    </body>
+</html>
